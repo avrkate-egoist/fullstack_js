3a64a8662b13bfd609e4d1adb4ee97d9b9056269
Katya Comp <avr.kate@gmail.com>
2026-02-23 18:18:19 +0200

    finish homework 31

--- async_await_31/main.js
+++ async_await_31/main.js
@@ -19,17 +19,36 @@ const inputCity = document.getElementById("inputCity");
 const checkButton = document.getElementById("checkButton");
 const result = document.getElementById("result");
 
-checkButton.addEventListener("click", () => {
+checkButton.addEventListener("click", async () => {
   const cityName = inputCity.value;
-  if (cityName) {
-    result.innerHTML = `City: ${cityName}`;
+  const data = await getWeather(cityName);
+  if (data.cod === 200) {
+    result.innerHTML = `city: ${data.name}<br>
+    Temp: ${data.main.temp} °C<br>
+    Pressure: ${data.main.pressure} hPa<br>
+    Description: ${data.weather[0].description}<br>
+    Humidity: ${data.main.humidity}%<br>
+    Speed: ${data.wind.speed} m/s, deg ${data.wind.deg}°<br>
+    <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"> `;
+  } else if (!data || data.cod === "404") {
+    result.innerHTML = "City is not found";
   } else {
-    result.innerHTML = `Something went wrong`;
+    result.innerHTML = "Something went wrong";
   }
 });
 
 async function getWeather(name) {
   const cityName = name.toUpperCase();
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=5d066958a60d315387d9492393935c19`,
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+  return null;
 }
 
 // За бажанням:
@@ -40,3 +59,42 @@ async function getWeather(name) {
 // По клику на кнопку коментарі має бути виведені нижче під постом коментарі до цього посту
 // Якщо зробити Пошук нового поста, старий пост та коментарі видаляються зі сторінки
 // Зробити завдання використовуючи проміси, перехопити помилки.
+
+const inputSearch = document.getElementById("inputSearch");
+const searchButton = document.getElementById("searchButton");
+const searchResult = document.getElementById("searchResult");
+
+searchButton.addEventListener("click", () => {
+  const infoSearch = parseInt(inputSearch.value);
+
+  if (isNaN(infoSearch)) {
+    searchResult.innerHTML = `Enter a number`;
+  } else if (infoSearch < 1 || infoSearch > 100) {
+    searchResult.innerHTML = `Enter a number from 1 to 100`;
+  } else if (infoSearch >= 1 && infoSearch <= 100) {
+    search(infoSearch);
+  }
+});
+
+async function search(num) {
+  searchResult.innerHTML = "";
+  const searchPost = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${num}`,
+  );
+  const datasearchPost = await searchPost.json();
+  searchResult.innerHTML = `<h3>${datasearchPost.title}</h3><br>
+  <p>${datasearchPost.body}</p><br>
+  <button id="commentsButton">Show comments</button>`;
+
+  const commentsButton = document.getElementById("commentsButton");
+  commentsButton.addEventListener("click", async () => {
+    const commentsResponse = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${num}/comments`,
+    );
+    const comments = await commentsResponse.json();
+    searchResult.innerHTML += "<h4>Comments:</h4>";
+    comments.forEach((comment) => {
+      searchResult.innerHTML += `<p>${comment.email}: ${comment.body}</p>`;
+    });
+  });
+}
