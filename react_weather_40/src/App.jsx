668f5619ab117029c6caa2ee87ee506c86e3c5f9
Kateryna Avramenko <avr.kate@gmail.com>
2026-03-28 12:58:35 +0300

    add homework 40

---
+++ react_weather_40/src/App.jsx
@@ -0,0 +1,11 @@
+import WeatherChecker from "./components/WeatherChecker";
+function App() {
+  return (
+    <>
+      <h1>Check your weather</h1>
+      <WeatherChecker />
+    </>
+  );
+}
+
+export default App;
