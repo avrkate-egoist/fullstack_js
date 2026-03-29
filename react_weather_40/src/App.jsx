89decc728d889541f8cf5541c61b1c44faf72081
Kateryna Avramenko <avr.kate@gmail.com>
2026-03-29 18:48:15 +0300

    ad second part of homewoork 40

--- react_weather_40/src/App.jsx
+++ react_weather_40/src/App.jsx
@@ -1,9 +1,16 @@
 import WeatherChecker from "./components/WeatherChecker";
+
+import PostChecker from "./components/PostChecker";
 function App() {
   return (
     <>
       <h1>Check your weather</h1>
       <WeatherChecker />
+      <hr />
+      <br />
+      <h1>Check your post</h1>
+
+      <PostChecker />
     </>
   );
 }
