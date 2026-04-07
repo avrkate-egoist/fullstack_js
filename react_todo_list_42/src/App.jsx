4c1e6340ff9369e2e0bd885d102206d627549f23
Kateryna Avramenko <avr.kate@gmail.com>
2026-04-07 18:21:53 +0300

    add homework 42

---
+++ react_todo_list_42/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import TodoList from "./components/TodoList.jsx";
+
+function App() {
+  return (
+    <>
+      <div>
+        <TodoList />
+      </div>
+    </>
+  );
+}
+
+export default App;
