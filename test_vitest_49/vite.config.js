17f78930f15998d1c2f344dca044444c6f5cfc10
Kateryna Avramenko <avr.kate@gmail.com>
2026-05-04 14:41:35 +0300

    add homework 49

---
+++ test_vitest_49/vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,jsx,ts,tsx}"],
+    exclude: ["node_modules"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.{js,jsx,ts,tsx}"],
+      exclude: ["node_modules", "tests"],
+    },
+  },
+});
