9a218e4b9beff0183ba899dbb1b5692fd41f1b7e
Kateryna Avramenko <avr.kate@gmail.com>
2026-07-20 16:26:37 +0300

    add homework 66

---
+++ mongo_db_crud_66/db.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectionString = process.env.MONGO_CONNECT_STR;
+const dbClient = new MongoClient(connectionString);
+
+export let db;
+
+export async function dbConnect() {
+  await dbClient.connect();
+  db = await dbClient.db("sample_mflix");
+  console.log("Connection successful");
+}
